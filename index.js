8629501b778e99862fdb56df3bab65d414f5a527
Matias Garcia <matiasigarcia91@gmail.com>
2020-06-11 11:42:43 +0200

    Add signup login + authmiddleware

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const authRouter = require("./auth/router");
+const authMiddleware = require("./auth/middleware");
+const User = require("./models").user;
 const PORT = 4000;
 
 const app = express();
 app.use(express.json()); // bodyParser
+
+app.use("/auth", authRouter);
+
+app.get("/users", authMiddleware, async (req, res, next) => {
+  try {
+    console.log("WHOS MAKING THE REQUEST?", req.user);
+    const users = await User.findAll();
+    res.send(users);
+  } catch (e) {
+    next(e);
+  }
+});
 
 app.listen(PORT, () => console.log("server started!"));
